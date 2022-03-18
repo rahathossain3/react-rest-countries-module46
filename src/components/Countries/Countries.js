4872ff9b355fdd754bb0ad63fee24c81b109ab5e
rahathossain3 <rahathossain210@gmail.com>
2022-03-19 05:52:01 +0600

    key add & layout added

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = () => {
     //1
@@ -13,20 +14,27 @@ const Countries = () => {
     }, [])
 
     return (
-        <div>
+        <div >
             <h1>Hello From Countries: {countries.length}</h1>
 
-            {
-                countries.map(country => console.log(country))
-            }
-            {
-                countries.map(country => <Country
-                    name={country.name.common}
-                    population={country.population}
-                    area={country.area}>
+            <div className='countries-container'>
+                {
+                    countries.map(country => <Country
+                        country={country}
+                        key={country.cca3}
+                    > </Country>)
+                }
+            </div>
 
-                </Country>)
-            }
+            {/* {  
+                countries.map(country => <Country
+                    country={country}
+                name={country.name.common}
+                population={country.population}
+                area={country.area}
+                region={country.region}
+                > </Country>)
+            } */}
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    // console.log(props)
+    const { area, region, population, name, flags } = props.country;
+    console.log(props.country)
     return (
         <div className="country">
-            <h2>Country Name: {props.name}</h2>
-            <p>Population: {props.population}</p>
-            <p><small>Area: {props.area}</small></p>
+            <h2>Country Name: {name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p><small>Region: {region}</small></p>
+            <p><small>Area: {area}</small></p>
         </div>
     );
 };
